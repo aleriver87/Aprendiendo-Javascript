0bad57fee116e9df8ece4b533c602fa5fdc90b25
aleriver87 <aleriver87@hotmail.com>
2018-02-21 17:41:54 -0600

    Actualizando

--- Practicas personales/js/main.js
+++ Practicas personales/js/main.js
@@ -1,50 +1,16 @@
-//Validando formulario
-/*$.validator.setDefaults({
-    
-    submitHandler: function(form ){
-
-        var username = $("#user").val();
-        var password = $("#password").val();
-
-        var user = {
-            username:username,
-            password:password
-        };
-
-        localStorage.setItem(username,JSON.stringify(user));
-
-        $("#user").val("");
-        $("#password").val("");
-    }
-});*/
-
-$(document).ready(function(){   
-   /* var validator = $("#login").validate({
-        errorPlacement: function(error, element){
-            $(element).closest("form")
-            .find("label[for='"+element.attr("id")+"']")
-            .prepend(error);
-        },
-        errorElement: "span",
-        messages: {
-            user:{
-                required: "Please enter your username"
-            },
-            password: {
-                required: "Please enter your password",
-                minlength: "Your answer should be {8} characters."
-            }
-        }
-    });*/
+$(document).ready(function(){
 
     $("#user").focus(function(){
         $("#user").removeAttr('placeholder');
+        $("#user").css('background-image','');
         $("#user_label").show();
         $("#user").css('opacity',0.7);
+        $("#user").css('font-size','1.2em');
       }).blur(function(){
-        $("#user").attr('placeholder','Username');
+        $("#user").attr('placeholder','Email address');
         $("#user_label").hide();
         $("#user").css('opacity',0.5);
+        $("#user").css('font-size','1em');
       });
 
     $("#password").focus(function(){
@@ -57,17 +23,4 @@ $(document).ready(function(){
         $("#password").css('opacity',0.5);
       });
 
-    /* Validando 
-    $("#user_error").hide();
-    var user = $("#user").val();
-    var password = $("#password").val();
-
-    $("#login_btn").click(function(){
-        if (user.trim() == "") {
-            $("#user_error").slideUp(10000);
-            $("#user_error").slideDown(10000);
-        } else{
-            
-        }
-    });*/
 });
---
+++ jQueryUI/js/progressbar.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#barra_deter").progressbar({
+        value:75
+    });
+    $("#barra_indet").progressbar({
+        value: false
+    });
+});
