8f99e1cde85a2a38a0a58d3d5aa38beed3ea1c5a
aleriver87 <aleriver87@hotmail.com>
2018-07-26 10:59:52 -0600

    Add files via upload

---
+++ AngularJS/Animaciones-2-Solucion/js/app.js
@@ -0,0 +1,18 @@
+var miApp = angular.module('miApp', ['ngRoute', 'ngAnimate']);
+
+miApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pagina-inicio.html',
+            controller: 'controllerInicio'
+        })
+        .when('/acercaDe', {
+            templateUrl: 'pagina-acercaDe.html',
+            controller: 'controllerAcercaDe'
+        })
+        .when('/contactenos', {
+            templateUrl: 'pagina-contactenos.html',
+            controller: 'controllerContactenos'
+        });
+
+});
---
+++ AngularJS/Animaciones-2-Solucion/js/controllers.js
@@ -0,0 +1,11 @@
+miApp.controller('controllerInicio', function($scope) {
+    $scope.pageClass = 'pagina-inicio';
+});
+
+miApp.controller('controllerAcercaDe', function($scope) {
+    $scope.pageClass = 'pagina-acercaDe';
+});
+
+miApp.controller('controllerContactenos', function($scope) {
+    $scope.pageClass = 'pagina-contactenos';
+});
